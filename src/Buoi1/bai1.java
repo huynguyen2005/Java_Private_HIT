97e0bac9baeaead9ba3d2b770147326a01174cf0
Harry <nguyenvanhuy12072005@gmail.com>
2024-10-15 19:02:28 +0700

    update

--- src/Buoi1/bai1.java
+++ src/Buoi1/bai1.java
@@ -1,7 +1,12 @@
 package Buoi1;
 
+import java.util.Scanner;
+
 public class bai1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		
 	}
 }

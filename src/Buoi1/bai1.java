e4f9e1645a8c8cfa40218e4b1f8fb34636ec6403
Harry <nguyenvanhuy12072005@gmail.com>
2024-10-15 19:06:09 +0700

    update

--- src/Buoi1/bai1.java
+++ src/Buoi1/bai1.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class bai1 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		Scanner sc = new Scanner(System.in);
-		int a=sc.nextInt();
+		int a = sc.nextInt();
+		System.out.println(a);
+		System.out.println("Hello World");
 		
 	}
 }
